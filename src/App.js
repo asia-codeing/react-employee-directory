5ff07c2e12b0ad84cba71c0cf6cf7ffaa3ff5ad5
Asia Alnahi <asia.alius@gmail.com>
2021-06-16 12:41:15 -0700

    Add Header and Employees to App.js file to return them

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Employees from "./components/Employees";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-      <h2>Asia Alnahi</h2>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <>
+      <Header/>
+      <Employees/>
+    </>  
   );
 }
-
 export default App;
